3ad505f1d444ab0103b9205de67dda3750362731
DevinKuhlman <devin.kuhlman@att.net>
2022-11-02 14:40:06 -0500

    Stopped at Metadata :)

---
+++ SATweb/SATweb.DATA.EF/Models/ScheduleClassStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SATweb.DATA.EF.Models
+{
+    public partial class ScheduleClassStatus
+    {
+        public ScheduleClassStatus()
+        {
+            ScheduledClasses = new HashSet<ScheduledClass>();
+        }
+
+        public int Scsid { get; set; }
+        public string Scsname { get; set; } = null!;
+
+        public virtual ICollection<ScheduledClass> ScheduledClasses { get; set; }
+    }
+}
